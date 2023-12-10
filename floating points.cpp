fdaf30844113c7e1069cc2c870f09b68ff78aeb2
gayathriprakerla <137159640+gayathriprakerla@users.noreply.github.com>
2023-12-10 19:04:45 +0530

    Add files via upload

---
+++ floating points.cpp
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	double a,b,result;
+	printf("enter a value:");
+	scanf("%lf",&a);
+	printf("enter b value:");
+	scanf("%lf",&b);
+	result=a*b;
+	printf("result= %.2f %lf",result);
+	getch();
+}
+
+	
